8713ac64ef09de6e1e44fcc6a219d001f64563f5
Jun Zhu <issaczhu0723@gmail.com>
2021-02-17 22:17:01 -0800

    commit

---
+++ ds_messenger.py
@@ -0,0 +1,21 @@
+class DirectMessage:
+    def __init__(self):
+        recipient = None
+        message = None
+        timestamp = None
+
+
+class DirectMessenger:
+    def __init__(self, dsuserver=None, username=None, password=None):
+        token = None
+
+    def send(self, message: str, recipient: str) -> bool:
+        pass
+
+    def retrieve_new(self) -> DirectMessage:
+        pass
+
+    def retrieve_all(self) -> list:
+        pass
+
+print("Hello World")
